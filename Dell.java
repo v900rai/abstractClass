1a4287161bc8282c465cac05a49a2b0365ea2f29
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2023-04-24 18:54:33 +0530

    Add files via upload

---
+++ Dell.java
@@ -0,0 +1,22 @@
+package edu.jaspiders.heca48.abstrack;
+
+
+	public class Dell extends Laptop
+	{
+		public  String name()
+		{
+			return "Hppp";
+	     }
+		public double price()
+		{
+			return 44499.44;
+		}
+		public  String colour()
+		{
+			return "black";
+		}
+
+	}
+
+
+
