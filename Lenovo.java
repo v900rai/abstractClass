d8817e2d7a51507555b4730faecae038cdcd21e2
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2023-04-24 18:55:34 +0530

    Add files via upload

---
+++ Lenovo.java
@@ -0,0 +1,18 @@
+package edu.jaspiders.heca48.abstrack;
+
+public class Lenovo extends Laptop
+{
+	public  String name()
+	{
+		return "Dell";
+     }
+	public double price()
+	{
+		return 444.44;
+	}
+	public  String colour()
+	{
+		return "Black";
+	}
+
+}
