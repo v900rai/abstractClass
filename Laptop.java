5002bc5410960a6997625ad0e6bc972db06561b8
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2023-04-24 18:55:14 +0530

    Add files via upload

---
+++ Laptop.java
@@ -0,0 +1,10 @@
+package edu.jaspiders.heca48.abstrack;
+public abstract class Laptop
+	 {
+		 public abstract String  name();
+		 public abstract double price();
+		 public abstract String colour();
+
+	}
+
+
